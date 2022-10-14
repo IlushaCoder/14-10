24c0d062e0cf07976869b2181e960672ae0b2994
BadAss <103047946+IlushaCoder@users.noreply.github.com>
2022-10-14 15:08:48 +0300

    Add files via upload

---
+++ divisors/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static String[] wave(String str) {
+        String[] words=new String[str.length()];
+
+    public static long numberOfDivisors(int n) {
+        long
+
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
